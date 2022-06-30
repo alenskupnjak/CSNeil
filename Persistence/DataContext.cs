4f47c0250d1a3647204e825a6270c601f8892f24
Alens <alenskupnjak@yahoo.com>
2022-06-30 23:53:59 +0200

    01 Dodani podaci u bazu

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions options) : base(options)
+        { 
+
+
+        }
+
+        public  DbSet<Activity> ActivitiesTable { get; set; }
+
+    }
+}
