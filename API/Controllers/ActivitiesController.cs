0dc05800e675a4ad2df5919c6b1c24e957775af0
Alens <alenskupnjak@yahoo.com>
2022-07-04 20:04:37 +0200

    07 - create detail

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -11,43 +11,43 @@ using System.Threading.Tasks;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    // 06 public class ActivitiesTableController : ControllerBase
-    public class ActivitiesTableController : BaseApiController
+  [ApiController]
+  [Route("api/[controller]")]
+  // 06 public class ActivitiesTableController : ControllerBase
+  public class ActivitiesTableController : BaseApiController
+  {
+
+    // 06  private readonly IMediator _mediator;
+
+    // 06 // 01 private readonly DataContext _context;
+
+    // 06  // 01 public ActivitiesTableController(DataContext context)
+    // 06 public ActivitiesTableController(IMediator mediator)
+    // 06  {
+    // 06    _mediator = mediator;
+    //    //  01 _context = context;
+    // 06 }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-       
-        // 06  private readonly IMediator _mediator;
 
-        // 06 // 01 private readonly DataContext _context;
+      // 01 return await _context.ActivitiesTable.ToListAsync();
 
-        // 06  // 01 public ActivitiesTableController(DataContext context)
-        // 06 public ActivitiesTableController(IMediator mediator)
-        // 06  {
-        // 06    _mediator = mediator;
-        //    //  01 _context = context;
-        // 06 }
-  
-        [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities()
-        {
+      // 06 return await _mediator.Send(new List.Query());
 
-           // 01 return await _context.ActivitiesTable.ToListAsync();
+      return await MediatorServis.Send(new List.Query());
 
-           // 06 return await _mediator.Send(new List.Query());
-
-            return await MediatorServis.Send(new List.Query());
-
-        }
-
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivitiy(Guid id)
-        {
-
-           // 01 return await _context.ActivitiesTable.FindAsync(id);
-            return Ok();
-
-        }
     }
+
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivitiy(Guid id)
+    {
+
+      // 01 return await _context.ActivitiesTable.FindAsync(id);
+      return await MediatorServis.Send(new Details.Query { Id = id });
+
+    }
+  }
 }
