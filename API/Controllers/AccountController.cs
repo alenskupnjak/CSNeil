a735ab646553ce8774878571ec4e58524654c89f
Alens <alenskupnjak@yahoo.com>
2022-08-01 18:28:20 +0200

    Kraj 136. Creating a token service

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using API.DTOs;
+using API.Services;
 using Domain;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -12,11 +13,13 @@ namespace API.Controllers
   {
     private readonly UserManager<AppUser> _userManager;
     private readonly SignInManager<AppUser> _signInManager;
+    private readonly TokenService _tokenService;
 
-    public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+    public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, TokenService tokenService)
     {
       _userManager = userManager;
       _signInManager = signInManager;
+      _tokenService = tokenService;
     }
 
     [HttpPost("login")]
@@ -35,7 +38,7 @@ namespace API.Controllers
         {
           DisplayName = user.DisplayName,
           Image = null,
-          Token = "Ovo je token",
+          Token = _tokenService.CreateToken(user),
           Username = user.UserName,
         };
       }
--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using API.Services;
 //using API.Services;
 using Domain;
 //using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -10,36 +11,37 @@ using Persistence;
 
 namespace API.Extensions
 {
-    public static class IdentityServiceExtensions
+  public static class IdentityServiceExtensions
+  {
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services,
+        IConfiguration config)
     {
-        public static IServiceCollection AddIdentityServices(this IServiceCollection services,
-            IConfiguration config)
-        {
-            services.AddIdentityCore<AppUser>(opt =>
-            {
-                opt.Password.RequireNonAlphanumeric = false;
-            })
-            .AddEntityFrameworkStores<DataContext>()
-            .AddSignInManager<SignInManager<AppUser>>();
+      services.AddIdentityCore<AppUser>(opt =>
+      {
+        opt.Password.RequireNonAlphanumeric = false;
+      })
+      .AddEntityFrameworkStores<DataContext>()
+      .AddSignInManager<SignInManager<AppUser>>();
 
-            services.AddAuthentication();
+      services.AddAuthentication();
+      services.AddScoped<TokenService>();
 
-            //var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+      //var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
 
-           // * services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-           //     .AddJwtBearer(opt =>
-           //     {
-           //         opt.TokenValidationParameters = new TokenValidationParameters
-           //         {
-           //             ValidateIssuerSigningKey = true,
-           //             IssuerSigningKey = key,
-           //             ValidateIssuer = false,
-           //             ValidateAudience = false
-           //       };
-           //     });
-           // services.AddScoped<TokenService>();  * //
+      // * services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+      //     .AddJwtBearer(opt =>
+      //     {
+      //         opt.TokenValidationParameters = new TokenValidationParameters
+      //         {
+      //             ValidateIssuerSigningKey = true,
+      //             IssuerSigningKey = key,
+      //             ValidateIssuer = false,
+      //             ValidateAudience = false
+      //       };
+      //     });
+      // services.AddScoped<TokenService>();  * //
 
-            return services;
-        }
+      return services;
     }
+  }
 }
